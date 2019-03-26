6fd3f64166e36b8d122cf99febe232d16934a4cd
kino <374932573@qq.com>
2019-03-26 22:35:19 +0800

    city-ajax

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -6,27 +6,7 @@
 				<div class="button-list">
 					<div class="button-wrapper">
 						<div class="button">
-							北京
-						</div>
-					</div>
-					<div class="button-wrapper">
-						<div class="button">
-							北京
-						</div>
-					</div>
-					<div class="button-wrapper">
-						<div class="button">
-							北京
-						</div>
-					</div>
-					<div class="button-wrapper">
-						<div class="button">
-							北京
-						</div>
-					</div>
-					<div class="button-wrapper">
-						<div class="button">
-							北京
+							 {{this.$store.state.city}}
 						</div>
 					</div>
 				</div>
@@ -34,73 +14,31 @@
 			<div class="area">
 				<div class="title border-topbottom">热门城市</div>
 				<div class="button-list">
-					<div class="button-wrapper">
-						<div class="button">
-							北京
-						</div>
-					</div>
-					<div class="button-wrapper">
-						<div class="button">
-							北京
-						</div>
-					</div>
-					<div class="button-wrapper">
-						<div class="button">
-							北京
-						</div>
-					</div>
-					<div class="button-wrapper">
-						<div class="button">
-							北京
-						</div>
-					</div>
-					<div class="button-wrapper">
-						<div class="button">
-							北京
-						</div>
+					<div 
+						class="button-wrapper" 
+						v-for="item of hot" 
+						:key="item.id"
+						@click="handleCityClick(item.name)"
+					>
+						<div class="button">{{item.name}}</div>
 					</div>
 				</div>
 			</div>
-			<div class="area">
-				<div class="title border-topbottom">A</div>
+			<div class="area" 
+				v-for="(item,key) of cities" 
+				:key="key"
+				:ref="key"	
+			>
+				<div class="title border-topbottom">{{key}}</div>
 				<div class="item-list">
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-				</div>
-			</div>
-			<div class="area">
-				<div class="title border-topbottom">A</div>
-				<div class="item-list">
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-				</div>
-			</div>
-			<div class="area">
-				<div class="title border-topbottom">A</div>
-				<div class="item-list">
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
-					<div class="item border-bottom">阿拉尔</div>
+					<div 
+						class="item border-bottom"
+						v-for="innerItem of item"
+						:key="innerItem.id"
+						@click="handleCityClick(innerItem.name)"
+					>
+						{{innerItem.name}}
+					</div>
 				</div>
 			</div>
 		</div>
@@ -110,8 +48,28 @@
 import Bscroll from 'better-scroll'
 export default {
 	name: 'CityList',
+	props: {
+		hot: Array,
+		cities: Object,
+		letter: String
+	},
+	methods: {
+		handleCityClick (city) {
+			this.$store.commit('changeCity', city);
+			this.$router.push('/');
+		}
+	},
 	mounted () {
 		this.scroll = new Bscroll(this.$refs.wrapper)
+	},
+	watch: {
+		letter () {
+			 if (this.letter) {
+				 const element = this.$refs[this.letter][0];
+				this.scroll.scrollToElement(element);
+				
+            }
+		}
 	}
 	
 }
